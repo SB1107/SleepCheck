565b6dad3eb3e3db8a5896b505443ecf104eb46d
Lim JaeHyo <jhLim@sbsolutions.co.kr>
2024-01-15 17:30:02 +0900

    메뉴 오류수정

--- app/src/main/java/kr/co/sbsolutions/newsoomirang/domain/repository/RemoteDataSource.kt
+++
@@ -1,9 +0,0 @@
-package kr.co.sbsolutions.newsoomirang.domain.repository
-
-import kotlinx.coroutines.flow.Flow
-import kr.co.sbsolutions.newsoomirang.data.entity.UserEntity
-import kr.co.sbsolutions.newsoomirang.domain.model.SnsLoginModel
-
-interface RemoteDataSource {
-    suspend fun postLogin(loginModel: SnsLoginModel): Flow<UserEntity>
-}
