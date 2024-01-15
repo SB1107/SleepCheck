1a99ae93c7ce89957d9a23299d299f477aa5889d
Lim JaeHyo <jhLim@sbsolutions.co.kr>
2024-01-15 18:58:34 +0900

    이미지 업로드 홈 프레그먼 트작업중

--- app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/main/breathing/BreathingFragment.kt
+++ app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/main/breathing/BreathingFragment.kt
@@ -1,18 +1,25 @@
 package kr.co.sbsolutions.newsoomirang.presenter.main.breathing
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kr.co.sbsolutions.newsoomirang.common.DataManager
 import kr.co.sbsolutions.newsoomirang.databinding.FragmentBreathingBinding
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class BreathingFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = BreathingFragment()
-    }
 
     private val viewModel: BreathingViewModel by viewModels()
     private val binding: FragmentBreathingBinding by lazy {
@@ -24,6 +31,19 @@ class BreathingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.userName.collectLatest {
+                    binding.tvName.text = it
+                }
+            }
+        }
+
     }
 
 }
--- app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/main/breathing/BreathingViewModel.kt
+++ app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/main/breathing/BreathingViewModel.kt
@@ -1,7 +1,30 @@
 package kr.co.sbsolutions.newsoomirang.presenter.main.breathing
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kr.co.sbsolutions.newsoomirang.common.DataManager
+import javax.inject.Inject
 
-class BreathingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class BreathingViewModel @Inject constructor(
+    private val dataManager: DataManager
+) : ViewModel() {
+    private val _userName: MutableSharedFlow<String> = MutableSharedFlow()
+    val userName: SharedFlow<String> = _userName
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            dataManager.getUserName().first()?.let {
+                _userName.emit(it)
+            }
+        }
+
+
+    }
 }
