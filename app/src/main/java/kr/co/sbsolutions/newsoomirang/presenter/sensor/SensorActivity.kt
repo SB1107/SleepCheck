54b923dec7de1ff9b2a5b6faeba11df9ec288963
Jungseongho <tjdgh25456@naver.com>
2024-01-17 11:23:22 +0900

    누락 내용 업로드

--- app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/sensor/SensorActivity.kt
+++ app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/sensor/SensorActivity.kt
@@ -1,38 +1,61 @@
 package kr.co.sbsolutions.newsoomirang.presenter.sensor
 
+import android.animation.ObjectAnimator
 import android.bluetooth.BluetoothDevice
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kr.co.sbsolutions.newsoomirang.databinding.ActivitySensorBinding
 import kr.co.sbsolutions.newsoomirang.presenter.BaseActivity
-import kr.co.sbsolutions.newsoomirang.presenter.BaseServiceActivity
 import kr.co.sbsolutions.newsoomirang.presenter.BaseViewModel
-import kr.co.sbsolutions.newsoomirang.presenter.splash.WHERE
 
+@AndroidEntryPoint
 class SensorActivity : BaseActivity() {
+
+    override fun newBackPressed() {
+        finish()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home -> {
+                newBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun injectViewModel(): BaseViewModel {
+        return viewModel
+    }
+
     private val viewModel: SensorViewModel by viewModels()
+
     private val binding: ActivitySensorBinding by lazy {
         ActivitySensorBinding.inflate(layoutInflater)
     }
-    private val bleAdapter : BluetoothAdapter by lazy {
+    private val bleAdapter: BluetoothAdapter by lazy {
         BluetoothAdapter(bleClickListener)
     }
 
     private val bleClickListener: (BluetoothDevice) -> Unit = { bluetoothDevice ->
-        viewModel.registerDevice( bluetoothDevice)
+        viewModel.registerDevice(bluetoothDevice)
+        binding.deviceNameTextView.text = bluetoothDevice.name.toString()
     }
-    override fun newBackPressed() {
-         finish()
-    }
-    override fun injectViewModel(): BaseViewModel {
-        return  viewModel
+
+    private val animator: ObjectAnimator by lazy {
+        ObjectAnimator.ofFloat(binding.btSearch, "rotation", 0f, 359f).apply {
+            duration = 1000L
+            repeatCount = ObjectAnimator.INFINITE
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,7 +72,6 @@ class SensorActivity : BaseActivity() {
                 adapter = bleAdapter
             }
 
-
             btSearch.setOnClickListener {
                 lifecycleScope.launch {
                     lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -57,6 +79,7 @@ class SensorActivity : BaseActivity() {
                             viewModel.isScanning.collectLatest {
                                 if (it) {
                                     Toast.makeText(this@SensorActivity, "스캔중", Toast.LENGTH_SHORT).show()
+                                    return@collectLatest
                                 }
                             }
                         }
@@ -64,22 +87,43 @@ class SensorActivity : BaseActivity() {
                 }
                 viewModel.scanBLEDevices()
             }
-
         }
 
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    viewModel.isRegistered.collectLatest {
-                        if (it) {
-                            finish()
-                        }else {
-                            Toast.makeText(this@SensorActivity, "재연결이 필요합니다. ", Toast.LENGTH_SHORT).show()
+        with(viewModel) {
+            lifecycleScope.launch {
+                lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+                    launch {
+                        isScanning.collectLatest { isScanning ->
+                            if (isScanning) {
+                                animator.start()
+
+                            } else {
+                                animator.cancel()
+
+                            }
+                        }
+                    }
+
+                    launch {
+                        isRegistered.collectLatest {
+                            if (it) {
+                                finish()
+                            } else {
+                                Toast.makeText(this@SensorActivity, "재연결이 필요합니다. ", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+
+                    launch {
+                        scanList.collectLatest { list ->
+                            bleAdapter.submitList(list)
                         }
                     }
                 }
             }
         }
+
     }
 
 
