ec7705ee0adcf40763a314b081c84d159dd7f471
Jungseongho <tjdgh25456@naver.com>
2024-01-25 12:06:26 +0900

    블루투스 활성화 하기

--- app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/BaseServiceActivity.kt
+++ app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/BaseServiceActivity.kt
@@ -1,35 +1,68 @@
 package kr.co.sbsolutions.newsoomirang.presenter
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
 import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
+import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.CallSuper
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kr.co.sbsolutions.newsoomirang.ApplicationManager
 import kr.co.sbsolutions.newsoomirang.BLEService
 import kr.co.sbsolutions.newsoomirang.BLEService.Companion.DATA_ID
+import kr.co.sbsolutions.newsoomirang.R
+import kr.co.sbsolutions.newsoomirang.common.showAlertDialogWithCancel
 import kr.co.sbsolutions.newsoomirang.presenter.main.ServiceCommend
 import kr.co.sbsolutions.withsoom.domain.bluetooth.entity.BluetoothState
 import java.lang.ref.WeakReference
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 abstract class BaseServiceActivity : BaseActivity() {
     private lateinit var service: WeakReference<BLEService>
+    private lateinit var bluetoothActivityResultLauncher: ActivityResultLauncher<Intent>
+
+    @Inject
+    lateinit var bluetoothAdapter: BluetoothAdapter
 
     override fun onStart() {
         super.onStart()
         bindService(Intent(this, BLEService::class.java), serviceConnection, BIND_AUTO_CREATE)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        onBluetoothActive()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         unbindService(serviceConnection)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!bluetoothAdapter.isEnabled) {
+            showAlertDialogWithCancel(R.string.common_title, "블루투스를 활성화가 필요합니다. \n활성화 하시겠습니까?",
+                confirmButtonText = R.string.setting_bluetooth_connect,
+                confirmAction = {
+                    requestBluetoothActivation()
+            },
+                cancelAction = {
+                    finish()
+                }
+            )
+        }
     }
 
     protected fun startSBService(am: ActionMessage, dataId: Int? = null) {
@@ -52,7 +85,7 @@ abstract class BaseServiceActivity : BaseActivity() {
                         } else if (info.bluetoothState == BluetoothState.Connected.Finish) {
                             changeServiceViewModel()?.setCommend(ServiceCommend.STOP)
                         }
-                        Log.e("Aa","call33")
+                        Log.e("Aa", "call33")
                         ApplicationManager.setBluetoothInfo(info)
                     }
                 }
@@ -93,6 +126,24 @@ abstract class BaseServiceActivity : BaseActivity() {
             service.clear()
             ApplicationManager.serviceClear()
         }
+    }
+
+    private fun onBluetoothActive() {
+        bluetoothActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // 사용자가 블루투스를 활성화했을 때의 처리
+
+            } else {
+                // 사용자가 블루투스를 활성화하지 않았을 때의 처리
+                finish()
+            }
+        }
+//        requestBluetoothActivation()
+    }
+
+    private fun requestBluetoothActivation() {
+        val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        bluetoothActivityResultLauncher.launch(enableBluetoothIntent)
     }
 
 }
