d2cac1d9a516ec5f7c03ff17c20a74f64226c8c7
Lim JaeHyo <jhLim@sbsolutions.co.kr>
2024-02-19 18:15:46 +0900

    호흡 측정 연결 필요시 글 속성 삭제 코골이  기기 연결 안했을시 진동 및 세기 디세이블

--- app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/main/nosering/NoSeringFragment.kt
+++ app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/main/nosering/NoSeringFragment.kt
@@ -252,6 +252,11 @@ class NoSeringFragment : Fragment() {
                     viewModel.bluetoothButtonState.collect {
                         binding.startButton.text = it
                         binding.tvNameDes2.text = if (it.contains("시작").not()) "숨이랑 기기와 연결이 되지 않았 습니다.\n기기와 연결해 주세요" else "시작버튼을 눌러\n코골이을 측정해 보세요"
+                        val isEnabled = it.contains("시작")
+                            binding.motorCheckBox.isEnabled = isEnabled
+                            binding.type0Chip.isEnabled = isEnabled
+                            binding.type1Chip.isEnabled = isEnabled
+                            binding.type2Chip.isEnabled = isEnabled
                     }
                 }
                 launch {
