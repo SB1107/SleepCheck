c0fd0c6f81ccb35f8d9f818f0014b7d4db68bc23
Lim JaeHyo <jhLim@sbsolutions.co.kr>
2024-02-26 09:40:52 +0900

    파싱 에러 방지 트라이 캐치

--- app/src/main/java/kr/co/sbsolutions/newsoomirang/common/AESHelper.kt
+++ app/src/main/java/kr/co/sbsolutions/newsoomirang/common/AESHelper.kt
@@ -16,29 +16,37 @@ class AESHelper {
     private val key = (BuildConfig.key).hexToBytes()
 
     fun encryptAES128(plainText: ByteArray): ByteArray {
+        return try {
             val tempByte = ByteArray(16).apply { fill(0.toByte(), 0, 16) }
             plainText.forEachIndexed { index, value ->
                 tempByte[index] = value
                 Log.d(TAG, "encryptAES128: ${tempByte[index]}")
             }
-//        val byttte = ByteArray(16).fill(0,plainText.size, 0.toByte())
             val cipher = Cipher.getInstance("AES/CBC/NoPadding")
                 .apply {
                     init(Cipher.ENCRYPT_MODE, SecretKeySpec(key, "AES"), IvParameterSpec(ivData))
                 }
             return cipher.doFinal(tempByte)
-
+        } catch (e: Exception) {
+            Log.e(TAG, "encryptAES128: ${e.message}")
+            return plainText
+        }
     }
 
     fun decryptAES128(encryptedText: ByteArray): ByteArray {
-        val cipher = Cipher.getInstance("AES/CBC/NoPadding")
-            .apply {
-                init(
-                    Cipher.DECRYPT_MODE, SecretKeySpec(key, "AES"),
-                    IvParameterSpec(ivData)
-                )
-            }
+        return try {
+            val cipher = Cipher.getInstance("AES/CBC/NoPadding")
+                .apply {
+                    init(
+                        Cipher.DECRYPT_MODE, SecretKeySpec(key, "AES"),
+                        IvParameterSpec(ivData)
+                    )
+                }
 
-        return cipher.doFinal(encryptedText)
+            return cipher.doFinal(encryptedText)
+        }catch (e : Exception){
+            Log.e(TAG, "decryptAES128: ${e.message}")
+            return encryptedText
+        }
     }
 }
