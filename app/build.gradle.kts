f3492eabea78aa48788c96755f072dd633195c03
Jungseongho <tjdgh25456@naver.com>
2024-03-29 17:08:19 +0900

    배포 버전

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "kr.co.sbsolutions.newsoomirang"
         minSdk = 26
         targetSdk = 34
-        versionCode = 24
-        versionName = "1.3.5"
+        versionCode = 25
+        versionName = "1.3.6"
         buildConfigField ("String", "SERVER_URL", "\"https://svc1.soomirang.kr/\"")
         buildConfigField ("String", "KAKAO",getProperties("kakao"))
         buildConfigField ("String", "ivData",getProperties("ivData"))
