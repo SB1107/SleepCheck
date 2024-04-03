c294072a5c6620fb1c9ad16157f936929f91c226
Lim JaeHyo <exit1190@gmail.com>
2024-04-03 15:11:54 +0900

    텍스트 사이즈 조절

--- app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/sensor/SensorActivity.kt
+++ app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/sensor/SensorActivity.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
 import android.view.MenuItem
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -22,7 +23,6 @@ import com.skydoves.balloon.overlay.BalloonOverlayAnimation
 import com.skydoves.balloon.overlay.BalloonOverlayRoundRect
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kr.co.sbsolutions.newsoomirang.R
@@ -97,10 +97,11 @@ class SensorActivity : BluetoothActivity() {
 
             tooltip = Balloon.Builder(this)
                 .setTextIsHtml(true)
+                .setAutoDismissDuration(2000)
                 .setHeight(BalloonSizeSpec.WRAP)
                 .setText(message)
                 .setTextColor(Color.BLACK)
-                .setTextSize(16f)
+                .setTextSize(24f)
                 .setTextLineSpacing(7f)
                 .setTextGravity(Gravity.START)
                 .setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
@@ -234,13 +235,9 @@ class SensorActivity : BluetoothActivity() {
     }
 
     private fun showToolTip() {
-        setToolTip("아래 센서를 선택하여 센서를 등록해 주세요")
+        setToolTip("아래 센서를 선택하여<br>센서를 등록해 주세요")
         lifecycleScope.launch(Dispatchers.Main) {
             tooltip.showAlignBottom(binding.deviceRecyclerView)
-            delay(5000)
-            if (::tooltip.isInitialized) {
-                tooltip.dismiss()
-            }
         }
     }
 }
