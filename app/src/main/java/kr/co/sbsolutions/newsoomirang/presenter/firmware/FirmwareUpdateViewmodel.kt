5301cc4f0a6ebe6a74f4b9b30fe3fd53378899e1
Jungseongho <tjdgh25456@naver.com>
2024-05-02 10:11:09 +0900

    UI 추가

--- app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/firmware/FirmwareUpdateViewmodel.kt
+++ app/src/main/java/kr/co/sbsolutions/newsoomirang/presenter/firmware/FirmwareUpdateViewmodel.kt
@@ -1,12 +1,16 @@
 package kr.co.sbsolutions.newsoomirang.presenter.firmware
 
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kr.co.sbsolutions.newsoomirang.common.DataManager
 import kr.co.sbsolutions.newsoomirang.common.TokenManager
+import kr.co.sbsolutions.newsoomirang.domain.bluetooth.entity.SBBluetoothDevice
 import kr.co.sbsolutions.newsoomirang.domain.repository.RemoteAuthDataSource
 import kr.co.sbsolutions.newsoomirang.presenter.BaseViewModel
 import javax.inject.Inject
@@ -30,8 +34,16 @@ class FirmwareUpdateViewmodel
     }
     
     fun checkDownloadInsertBtn() {
-        //
         }
     
+    fun deviceInfo():Pair<String, String> {
+        var deviceName :String = ""
+        var deviceAddress : String = ""
+        viewModelScope.launch {
+            deviceName = dataManager.getBluetoothDeviceName(SBBluetoothDevice.SB_SOOM_SENSOR.type.name).first().toString()
+            deviceAddress = dataManager.getBluetoothDeviceAddress(SBBluetoothDevice.SB_SOOM_SENSOR.type.name).first().toString()
+        }
+        return Pair(second = deviceAddress, first = deviceName)
     }
+}
     
