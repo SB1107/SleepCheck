4a9b11ee65846e7b4ce563991904a1b6983a65bf
Lim JaeHyo <exit1190@gmail.com>
2024-06-03 16:58:46 +0900

    데이터 베이스 초기화 앱 아이콘 변경 로고 밴경 앱 이름 변경

--- app/src/main/java/kr/co/sbsolutions/sleepcheck/data/db/SBSensorDataBase.kt
+++ app/src/main/java/kr/co/sbsolutions/sleepcheck/data/db/SBSensorDataBase.kt
@@ -12,7 +12,7 @@ import kr.co.sbsolutions.sleepcheck.domain.db.SettingDao
 import kr.co.sbsolutions.soomirang.db.LogData
 import kr.co.sbsolutions.soomirang.db.SBSensorData
 
-@Database(entities = [SBSensorData::class,LogData::class, SettingData::class] , version = 3)
+@Database(entities = [SBSensorData::class,LogData::class, SettingData::class] , version = 1)
 abstract class SBSensorDataBase  : RoomDatabase(){
     abstract  fun logDataDao() : LogDataDao
     abstract fun sbSensorDAO() : SBSensorDataDao
@@ -24,30 +24,9 @@ abstract class SBSensorDataBase  : RoomDatabase(){
         fun getDatabase(context:  Context) : SBSensorDataBase = instance ?: synchronized(this) {
             instance ?: buildDatabase(context).also { instance = it }
         }
-        private val MIGRATION_1_2 = object : Migration(1, 2) {
-            override fun migrate(db: SupportSQLiteDatabase) {
-                db.execSQL("CREATE TABLE New_Setting (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, sleepType TEXT NOT NULL, snoringOnOff INTEGER NOT NULL, snoringVibrationIntensity INTEGER NOT NULL, dataId INTEGER)")
-                db.execSQL(
-                    "INSERT INTO New_Setting (id, sleepType, snoringOnOff, snoringVibrationIntensity, dataId) " +
-                            "SELECT id, sleepType, snoringOnOff, snoringVibrationIntensity,null FROM Setting"
-                )
-                db.execSQL("DROP TABLE Setting")
-                db.execSQL("ALTER TABLE New_Setting RENAME TO Setting")
 
-            }
-        }
-
-        private  val MIGRATION_2_3 = object : Migration(2,3) {
-            override fun migrate(db: SupportSQLiteDatabase) {
-                db.execSQL("CREATE TABLE NEW_SLEEP_DATA ( id INTEGER PRIMARY KEY NOT NULL , time TEXT NOT NULL, capacitance INTEGER NOT NULL, calcAccX TEXT NOT NULL, calcAccY TEXT NOT NULL, calcAccZ TEXT NOT NULL, dataId INTEGER  NOT NULL DEFAULT -1)")
-                db.execSQL("DROP TABLE SLEEP_DATA")
-                db.execSQL("ALTER TABLE NEW_SLEEP_DATA RENAME TO SLEEP_DATA")
-            }
-        }
 
         private fun buildDatabase(appContext: Context) = Room.databaseBuilder(appContext, SBSensorDataBase::class.java, "sb_sensor.db")
-            .addMigrations(MIGRATION_1_2)
-            .addMigrations(MIGRATION_2_3)
             .build()
     }
 
