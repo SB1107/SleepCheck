8cdec309824f5bf024fd3879e9d315cd03a37e47
Lim JaeHyo <exit1190@gmail.com>
2024-06-10 16:02:30 +0900

    수면체크 버전코드 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "kr.co.sbsolutions.sleepcheck"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
         buildConfigField ("String", "SERVER_URL", "\"https://svc1.soomirang.kr/\"")
         buildConfigField ("String", "KAKAO",getProperties("kakao"))
